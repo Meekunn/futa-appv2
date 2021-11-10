b81b6efefa79ba76a23f4d79e9c7176bfd36ae27
Meekunn <orofinadedamola@gmail.com>
2021-11-10 01:26:48 +0100

    .env created and in use

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBcJaAphV6y8WA1Y1q1EuDanCD5L5ygYNY",
-    authDomain: "my-app-e4b5c.firebaseapp.com",
-    projectId: "my-app-e4b5c",
-    storageBucket: "my-app-e4b5c.appspot.com",
-    messagingSenderId: "244094676353",
-    appId: "1:244094676353:web:837586e50b154b32724c80",
-    measurementId: "G-FHGB5DYQNS",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig);
